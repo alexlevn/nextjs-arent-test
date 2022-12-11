7b514c9d413af423a5a17af6daa361e5e368fe61
Alex Le <thanhdungvn@gmail.com>
2022-12-12 01:00:52 +0700

    body-record

--- src/containers/MyRecords/BodyRecord.tsx
+++ src/containers/MyRecords/BodyRecord.tsx
@@ -1,40 +1,16 @@
-import { FC, useState } from 'react'
-
-interface IRecord {
-  name: string
-  kcal: number
-  time: number
-}
-
-const recordItem: IRecord = {
-  name: '家事全般（立位・軽い）',
-  kcal: 345,
-  time: 12,
-}
-
-const arrItems = Array(50).fill(recordItem)
+import { FC } from 'react'
+import Chart from 'src/components/Chart'
 
 const BodyRecord: FC = () => {
-  const [data, _] = useState<IRecord[]>(arrItems)
   return (
     <div className="bg-pc_gray text-white p-5">
-      <h2 className="uppercase font-sans">Body Record</h2>
-      <div className="grid grid-cols-2 gap-x-10 max-h-56 overflow-auto pr-6">
-        {data.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className="flex gap-2 border-b font-sans border-white py-1 "
-            >
-              *{' '}
-              <div className="flex-1">
-                <div className="">{item.name}</div>
-                <div className="text-pc_yellow">{item.kcal} kcal</div>
-              </div>
-              <p className="h6 text-pc_yellow">{item.time} min</p>
-            </div>
-          )
-        })}
+      <h2 className="w-1/4 flex justify-between items-center uppercase font-sans text-sm mb-3">
+        BODY <br /> RECORD
+        <span className="text-2xl font-extralight">2022.12.12</span>
+      </h2>
+
+      <div className="bg-pc_dark_gray">
+        <Chart />
       </div>
     </div>
   )
--- src/containers/MyRecords/MyExercise.tsx
+++ src/containers/MyRecords/MyExercise.tsx
@@ -1,9 +1,45 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+
+interface IRecord {
+  name: string
+  kcal: number
+  time: number
+}
+
+const recordItem: IRecord = {
+  name: '家事全般（立位・軽い）',
+  kcal: 345,
+  time: 12,
+}
+
+const arrItems = Array(50).fill(recordItem)
 
 const MyExercise: FC = () => {
+  const [data, _] = useState<IRecord[]>(arrItems)
   return (
-    <div>
-      <h1>My Exercise</h1>
+    <div className="bg-pc_gray text-white p-5 ">
+      <h2 className="w-1/4 flex justify-between items-center uppercase font-sans text-sm mb-3">
+        My <br /> Exercise
+        <span className="text-2xl font-extralight">2022.12.12</span>
+      </h2>
+      
+      <div className="grid grid-cols-2 gap-x-10 max-h-56 overflow-auto pr-6">
+        {data.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="flex gap-2 border-b font-sans border-white py-1 "
+            >
+              *{' '}
+              <div className="flex-1">
+                <div className="">{item.name}</div>
+                <div className="text-pc_yellow">{item.kcal} kcal</div>
+              </div>
+              <p className="h6 text-pc_yellow">{item.time} min</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
