c390952b9e737957bd913da74a5912ad7238681e
Alex Le <thanhdungvn@gmail.com>
2022-12-11 13:13:17 +0700

    save

--- src/components/Icon/index.tsx
+++
@@ -1,6 +0,0 @@
-import ProfileIcon from './images/profile.svg'
-import LogoIcon from './images/profile.svg'
-import NewsIcon from './images/news.svg'
-import ChallengeIcon from './images/challenge.svg'
-
-export { ProfileIcon, LogoIcon, NewsIcon, ChallengeIcon }
--- src/components/Layout/TopMenu/index.tsx
+++ src/components/Layout/TopMenu/index.tsx
@@ -1,17 +1,45 @@
-import { LogoIcon } from "src/components/Icon"
+import Image from 'next/legacy/image'
 
 const TopMenu = () => {
+  const arrTopMenuItems = [
+    {
+      id: 1,
+      name: 'Home',
+      title: 'Home',
+      icon: '/images/profile.svg',
+      link: '/',
+    },
+    {
+      id: 2,
+      name: 'About',
+      title: 'Home',
+      link: '/about',
+      icon: '/images/challenge.svg',
+    },
+    {
+      id: 3,
+      name: 'Contact',
+      title: 'Home',
+      link: '/contact',
+      icon: '/images/news.svg',
+    },
+  ]
   return (
     <div className="w-full bg-pc_gray">
-      <div className="max-w-desktop flex gap-10 my-5">
-        
-        <LogoIcon/>
+      <div className="max-w-desktop flex gap-10 my-3">
+        {/* <img src = "/images/logo.png" alt = "logo" className = "w-20 h-20" /> */}
+        {/* use Legacy Image in NextJs */}
 
-
-        
-        <div className="flex gap-2 items-center">
-
+        <div className="logo">
+          <Image
+            src="/images/healthy_logo.svg"
+            alt="logo"
+            width={150}
+            height={100}
+          />
         </div>
+
+        <div className="flex gap-5 items-center"></div>
       </div>
     </div>
   )
